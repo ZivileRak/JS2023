1251d36618a8f864e2372601a74a175ac92b7663
Zivile Rakickaite <zivile.rakickaite@gmail.com>
2023-11-09 19:15:25 +0200

    next version

--- Project/web-store/src/HeadPages/Product.jsx
+++ Project/web-store/src/HeadPages/Product.jsx
@@ -1,70 +1,58 @@
 import React, { useState, useEffect } from 'react'
 import './styles.css';
+import {ShopContext} from './Spro.jsx';
 
+  const Product= () => {
+  const [Data, setData]= useState([])
+  useEffect(()=>{
+    fetch('http://localhost:5000/product')
+    .then(res=>res.json())
+    .then(data => setData(data))
+    .catch(error => console.log(error));
+  },[])
 
+  return (
+  
+  <div>
 
-const Product= () => {
-return (
-    <div>
-      <header className="bg-dark text-white text-center py-3">
+<header className="bg-dark text-white text-center py-3">
         <h1>SPA equipment webshop </h1>
-        <h3>Enjoy SPA at home </h3>
-        /*<nav className="mt-3">
-          <ul className="nav">
-          <li className="nav-item"><a className="nav-link" href="#">Home</a></li>
-            <li className="nav-item"><a className="nav-link" href="#">SPA equitment</a></li>
-            <li className="nav-item"><a className="nav-link" href="#">Shopping cart</a></li>
-            <li className="nav-item"><a className="nav-link" href="#">Login</a></li>
-          </ul>
-        </nav>*/
+        <h3>Enjoy SPA at home </h3>  
       </header>
 
-      <main className="container mt-4">
-        <section id="product-list">
-          <div className="row">
-            <div className="col-md-4">
-              <div className="card mb-4">
-                <div className="card-body">
-                  <h2 className="card-title">Hydromassage Pool SPA “Relax”</h2>
-                  <p className="card-text">Unique new spa pool with 3 full laying seats and 2 semi-laying seats.</p>
-                  <button className="btn btn-primary">Order</button>
-                </div>
-              </div>
-            </div>
-        </div>
-            {/* Galite pridėti daugiau produktų čia */}
-            <div className="row">
-            <div className="col-md-4">
-              <div className="card mb-4">
-                <div className="card-body">
-                  <h2 className="card-title">Hydromassage Pool SPA "Recharge"</h2>
-                  <p className="card-text">Classic-style round spa pool for up to 5 people</p>
-                  <button className="btn btn-primary">Order</button>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-md-4">
-              <div className="card mb-4">
-                <div className="card-body">
-                  <h2 className="card-title">Hydromassage Pool SPA "Activity"</h2>
-                  <p className="card-text">Compact swimming spa with 3 massage seats and an adjustable river swimming system that allows you to swim energetically while keeping your head comfortably above water level.</p>
-                  <button className="btn btn-primary">Order</button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
-      </main>
+    <table className="container mt-4">
+      <thead>
+     
+        <th> Product Name</th>
+        <th> Product Description</th>
+        <th> Price</th>
+    
+      </thead>
+      
+      <tbody>
+      
+       
+        {Data.map((d,i)=> (
+          <tr key ={i}>
+            <td>{d.ProductID}</td>
+            <td>{d.ProductName}</td>
+            <td>{d.ProductDescription}</td>
+            <td>{d.Price}</td>
 
-      <footer className="bg-dark text-white text-center py-2">
-        <p>&copy; 2023 Web shop</p>
-      </footer>
-    </div>
+            <button className="btn btn-primary">Order</button>
+       
+          </tr>
+          
+        ))}
+      </tbody>
+     
+    </table>
+ 
+        <footer className="bg-dark text-white text-center py-2">
+          <p>&copy; 2023 Web Shop</p>
+        </footer> 
+  </div>
   );
 }
 
 export default Product;
-
-
